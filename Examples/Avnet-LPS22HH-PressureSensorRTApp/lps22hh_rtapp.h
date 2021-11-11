6b1a8a0d5887f4b139585a3d193f24a065e25f4a
Brian W <brian@willess.com>
2021-11-11 14:56:51 -0500

    LPS22HH RTApp2 configuration

--- Examples/Avnet-LPS22HH-PressureSensorRTApp/lps22hh_rtapp.h
+++ Examples/Avnet-LPS22HH-PressureSensorRTApp/lps22hh_rtapp.h
@@ -3,23 +3,38 @@
 
 #pragma once
 
+#include "intercore_generic.h"
+
 // Define the different messages IDs we can send to real time applications
 // If this enum is changed, it also needs to be changed for the high level application
-typedef enum   __attribute__((packed))
+typedef enum __attribute__((packed))
 {
-	IC_UNKNOWN, 
-    IC_HEARTBEAT,
-	IC_READ_SENSOR, 
-	IC_READ_SENSOR_RESPOND_WITH_TELEMETRY, 
-	IC_SET_SAMPLE_RATE
-} INTER_CORE_CMD;
+	IC_LPS22HH_UNKNOWN, 
+    IC_LPS22HH_HEARTBEAT,
+	IC_LPS22HH_READ_SENSOR_RESPOND_WITH_TELEMETRY, 
+	IC_LPS22HH_SET_SAMPLE_RATE,
+	/////////////////////////////////////////////////////////////////////////////////
+	// Don't change the enums above or the generic RTApp implementation will break //
+	/////////////////////////////////////////////////////////////////////////////////
+	IC_LPS22HH_READ_SENSOR, 
 
-// Define the expected data structure.  Note this struct came from the AvnetGroveGPS real time application code
+} INTER_CORE_CMD_LPS22HH;
+
+// Define the data structure that the high level app sends
 typedef struct  __attribute__((packed))
 {
-    INTER_CORE_CMD cmd;
+    INTER_CORE_CMD_LPS22HH cmd;
     uint32_t sensorSampleRate;
-    float pressure;
-    char telemetryJSON[64];
-} IC_COMMAND_BLOCK_LPS22HH;
+} IC_COMMAND_BLOCK_LPS22HH_HL_TO_RT;
 
+// Define the data structure that the real time app sends
+typedef struct  __attribute__((packed))
+{
+    INTER_CORE_CMD_LPS22HH cmd;
+    uint32_t sensorSampleRate;
+    char telemetryJSON[JSON_STRING_MAX_SIZE];
+	////////////////////////////////////////////////////////////////////////////////////////
+	// Don't change the declarations above or the generic RTApp implementation will break //
+	////////////////////////////////////////////////////////////////////////////////////////
+    float pressure;
+} IC_COMMAND_BLOCK_LPS22HH_RT_TO_HL;
