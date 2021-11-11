e9e2922fa24041350e21f5461076c8cab303ea8d
Brian W <brian@willess.com>
2021-11-11 10:45:01 -0500

    HTU21D working with generic interface

--- Examples/Avnet-HTU21D-TempHumRTApp/htu21d_rtapp.h
+++ Examples/Avnet-HTU21D-TempHumRTApp/htu21d_rtapp.h
@@ -3,26 +3,32 @@
 
 #pragma once
 
+#include "intercore_generic.h"
+
 // Define the different messages IDs we can send to real time applications
 // If this enum is changed, it also needs to be changed for the high level application
 typedef enum   __attribute__((packed))
 {
 	IC_TEMPHUM_UNKNOWN, 
     IC_TEMPHUM_HEARTBEAT,
-    IC_TEMPHUM_READ_TEMP_HUM_SENSOR, 
 	IC_TEMPHUM_READ_SENSOR_RESPOND_WITH_TELEMETRY, 
 	IC_TEMPHUM_SET_TELEMETRY_SEND_RATE,    
-} INTER_CORE_CMD_TEMPHUM;
+    IC_TEMPHUM_READ_TEMP_HUM_SENSOR
 
-#define RT_TELEMETRY_BUFFER_SIZE 64
+} INTER_CORE_CMD_TEMPHUM;
 
 // Define the expected data structure. 
 typedef struct  __attribute__((packed))
 {
     INTER_CORE_CMD_TEMPHUM cmd;
     uint32_t telemtrySendRate;
+} IC_COMMAND_BLOCK_TEMPHUM_HL_TO_RT;
+
+typedef struct  __attribute__((packed))
+{
+    INTER_CORE_CMD_TEMPHUM cmd;
+    uint32_t telemtrySendRate;
+    char telemetryJSON[JSON_STRING_MAX_SIZE];
     float temp;
     float hum;
-    char telemetryJSON[RT_TELEMETRY_BUFFER_SIZE];
-} IC_COMMAND_BLOCK_TEMPHUM;
-
+} IC_COMMAND_BLOCK_TEMPHUM_RT_TO_HL;
